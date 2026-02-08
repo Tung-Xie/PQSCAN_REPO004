6b763f6f417faf1e3f640239bcd49f2d8516b98c
dub dub Wubba lubba <tonyhsieh606@gmail.com>
2026-02-08 17:49:18 +0800

    update: experimental lab code

---
+++ research_lab/experimental_lattice.py
@@ -0,0 +1,6 @@
+def lattice_test():
+    # Experimental Lattice-based algorithms
+    target = "FrodoKEM-640-AES"
+    signature = "Falcon-1024"
+    hash_func = "SHAKE256"
+    print(f"Testing {target} with {signature}")
