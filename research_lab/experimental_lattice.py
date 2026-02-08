96f4acff670093db7343b7aad4325aae4f637d4f
Tung-Xie <tony.xie@chelpis.com>
2026-02-09 01:35:05 +0800

    Delete research_lab directory

--- research_lab/experimental_lattice.py
+++
@@ -1,6 +0,0 @@
-def lattice_test():
-    # Experimental Lattice-based algorithms
-    target = "FrodoKEM-640-AES"
-    signature = "Falcon-1024"
-    hash_func = "SHAKE256"
-    print(f"Testing {target} with {signature}")
