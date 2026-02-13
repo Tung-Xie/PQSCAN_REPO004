141df2b4bcb7edfa1e6f92394990dabeeffeb25d
Tung-Xie <tony.xie@chelpis.com>
2026-02-13 22:16:30 +0800

    Delete REPO004 directory

--- REPO004/app/utils/crypto/ciphers.py
+++
@@ -1,6 +0,0 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-def run_ciphers():
-    # Cipher: 3DES and Blowfish
-    c1 = Cipher(algorithms.TripleDES(b'1234567812345678'), modes.CBC(b'12345678'))
-    c2 = Cipher(algorithms.Blowfish(b'static_key'), modes.ECB())
-    return c1, c2
