e5606379fa02252f58527940b512ab410320f063
Tung-Xie <tony.xie@chelpis.com>
2026-02-13 22:16:58 +0800

    Add files via upload

---
+++ src/quantum_gateway/hybrid/advanced_logic.py
@@ -0,0 +1,7 @@
+class QuantumHybridCore:
+    def __init__(self):
+        self.X25519_MLKEM768 = "x25519_kyber768"
+        self.P256_MLKEM512 = "p256_kyber512"
+        self.SNOVA = "snova2454"
+        self.MAYO = "mayo1"
+core = QuantumHybridCore()
