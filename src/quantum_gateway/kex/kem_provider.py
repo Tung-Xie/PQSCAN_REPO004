94d6fa72df65cb025be83bb4cc087ab9febd91d8
Tung-Xie <tony.xie@chelpis.com>
2026-02-14 18:21:20 +0800

    Delete src/quantum_gateway directory

--- src/quantum_gateway/kex/kem_provider.py
+++
@@ -1,8 +0,0 @@
-import oqs
-KEM_ML_KEM_512 = oqs.KeyEncapsulation("Kyber512")
-KEM_ML_KEM_768 = oqs.KeyEncapsulation("Kyber768")
-KEM_ML_KEM_1024 = oqs.KeyEncapsulation("Kyber1024")
-KEM_FRODO_640_AES = oqs.KeyEncapsulation("FrodoKEM-640-AES")
-KEM_BIKE_L1 = oqs.KeyEncapsulation("BIKE-L1")
-KEM_HQC_128 = oqs.KeyEncapsulation("HQC-128")
-print("KEM_LOADED")
