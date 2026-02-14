0f47ed71a74337ba0ebb5fe142ff2f0e5b56f591
Tung-Xie <tony.xie@chelpis.com>
2026-02-14 18:13:24 +0800

    Add files via upload

---
+++ src/quantum_gateway/sign/sig_provider.py
@@ -0,0 +1,7 @@
+import oqs
+SIG_ML_DSA_44 = oqs.Signature("Dilithium2")
+SIG_ML_DSA_65 = oqs.Signature("Dilithium3")
+SIG_ML_DSA_87 = oqs.Signature("Dilithium5")
+SIG_FALCON_512 = oqs.Signature("Falcon-512")
+SIG_SPHINCS = oqs.Signature("SPHINCS+-SHA2-128f-simple")
+print("SIG_LOADED")
